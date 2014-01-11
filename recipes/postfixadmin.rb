c3ee6b89555bb0de831baee7ca9240ed44baf4bf
Mike Kruk <tamagokun@gmail.com>
2014-01-10 22:54:48 -0500

    Update postfixadmin.rb

--- recipes/postfixadmin.rb
+++ recipes/postfixadmin.rb
@@ -16,12 +16,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "lighttpd::php"
+# include_recipe "lighttpd::php"
 
 VERSION = "2.3.5"
 
 # Dependencies
-%w(php5-imap php5-pgsql).each do |pkg|
+%w(php5-imap php5-pgsql php5-fpm).each do |pkg|
   package pkg
 end
 
@@ -29,25 +29,18 @@ remote_file "#{Chef::Config[:file_cache_path]}/postfixadmin-#{VERSION}.tar.gz" d
   source "http://sourceforge.net/projects/postfixadmin/files/postfixadmin/postfixadmin-#{VERSION}/postfixadmin-#{VERSION}.tar.gz/download"
   checksum "9a72ed8d827fa2c7f641001f2aa87814"
   mode "0644"
-  not_if { File.exists?("/var/www/postfixadmin-#{VERSION}") }
+  not_if { File.exists?("/web/postfixadmin-#{VERSION}") }
   notifies :run, "execute[unpack postfixadmin]", :immediately
 end
 
 execute "unpack postfixadmin" do
-  command "tar xzvf #{Chef::Config[:file_cache_path]}/postfixadmin-#{VERSION}.tar.gz -C /var/www"
+  command "tar xzvf #{Chef::Config[:file_cache_path]}/postfixadmin-#{VERSION}.tar.gz -C /web"
   action :nothing
 end
 
-template "/var/www/postfixadmin-#{VERSION}/config.local.php" do
+template "/web/postfixadmin-#{VERSION}/config.local.php" do
   source "postfixadmin/config.local.php.erb"
   mode 0644
   owner "www-data"
   group "www-data"
-end
-
-lighttpd_vhost "postfixadmin" do
-  host "admin.#{node[:fqdn]}"
-  document_root "/var/www/postfixadmin-#{VERSION}"
-  action [:create, :enable]
-  notifies :restart, "service[lighttpd]"
 end
