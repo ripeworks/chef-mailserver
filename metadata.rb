5a438b45c949861c2906c5b38d603b8c6e14bf39
Mike Kruk <tamagokun@gmail.com>
2014-01-10 22:58:48 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ description      "Installs and configures a full mail server stack with postfix,
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 depends          "database"
 depends          "lighttpd"
-version          "0.1.0"
+version          "0.1.1"
